084002000d11b879738eb789f50a61fdb56ed2a0
treezstephen <stephen@treez.io>
2020-06-21 00:12:00 -0700

    Tutorial Finished

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { itemsRouter } from "./items/items.router";
+import { errorHandler } from "./middleware/error.middleware";
+import {notFoundHandler} from "./middleware/notFound.middleware";
 
 dotenv.config();
 
@@ -28,6 +31,10 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+app.use("/items", itemsRouter);
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 /**
  * Server Activation
